3910f0bc311a82f6e9093fc2288ceca9805ca4d0
haris <talhahkhaan@gmail.con>
2025-11-07 20:51:16 +0500

    ExternalDb-Setup

---
+++ Migrations/20251107152429_ExternalSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExternalSource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
