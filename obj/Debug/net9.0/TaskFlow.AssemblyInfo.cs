4e6d68e2311687c7987123a691df78532975dedb
haris <talhahkhaan@gmail.con>
2025-11-07 21:06:00 +0500

    SingupApiSetup

--- obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3910f0bc311a82f6e9093fc2288ceca9805ca4d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46627039b9d8b7ae93d5a6e5cb1724ecc66f7a9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
