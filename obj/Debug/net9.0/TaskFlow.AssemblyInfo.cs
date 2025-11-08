dc0ed1c221005d8f67b449767d20cb1808b9e945
haris <talhahkhaan@gmail.con>
2025-11-08 18:28:24 +0500

    AutoMapper , Login Api (Ready)

--- obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6d68e2311687c7987123a691df78532975dedb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65eb209bbb0c03c9bd06e10f046a54b7f240b67")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
